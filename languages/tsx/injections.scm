10af283234c5bb44e62190bdcd35917081ecf677
L Lllvvuu <git@llllvvuu.dev>
2024-03-03 20:13:04 -0800

    feat: typescript/tsx injections

---
+++ languages/tsx/injections.scm
@@ -0,0 +1,26 @@
+(call_expression
+  function: (identifier) @_name (#eq? @_name "graphql")
+  arguments:
+    (arguments
+      (template_string) @content
+        (#set! "include-children")
+        (#set! "language" "graphql")))
+(call_expression
+  function: (identifier) @_name (#eq? @_name "gql")
+  arguments:
+    (arguments
+      (template_string) @content
+        (#set! "include-children")
+        (#set! "language" "graphql")))
+(call_expression
+  function: (identifier) @_name (#eq? @_name "gql")
+  arguments:
+    ((template_string) @content
+      (#set! "include-children")
+      (#set! "language" "graphql")))
+(call_expression
+  function: (identifier) @_name (#eq? @_name "graphql")
+  arguments:
+    ((template_string) @content
+      (#set! "include-children")
+      (#set! "language" "graphql")))
